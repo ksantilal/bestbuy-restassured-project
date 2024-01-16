b102a77a7fe42d02786efa38a1306f7b41d57786
kapilsantilal <kapilbamania96@gmail.com>
2024-01-16 20:34:15 +0000

    Homework/bestbuy-restassured-project

---
+++ src/test/java/com/restassured/bestbuy/testbase/TestBase.java
@@ -0,0 +1,14 @@
+package com.restassured.bestbuy.testbase;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+public class TestBase {
+
+    @BeforeClass
+    public void inIt() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3030;
+        RestAssured.basePath = "/stores";
+    }
+}
